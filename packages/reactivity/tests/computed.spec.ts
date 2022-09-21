53f915f94b82186b6630dd893bf873ce71256dd0
unbrain <marsorsun@gmail.com>
2022-09-21 23:04:57 +0800

    feat(computed): add computed

---
+++ packages/reactivity/tests/computed.spec.ts
@@ -0,0 +1,44 @@
+import { describe, expect, it, vi } from "vitest";
+import { reactive } from "../reactive";
+import { computed } from "../computed";
+
+describe('computed', () => {
+  it('hh', () => {
+    const user = reactive({age: 1});
+    const age = computed(() => {
+      return user.age;
+    });
+
+    expect(age.value).toBe(1);
+  });
+
+  it('should computed lazily', () => {
+    const value = reactive({
+      foo: 1,
+    })
+
+    const getter = vi.fn(() => {
+      return value.foo;
+    });
+
+    const computedValue = computed(getter); 
+
+    expect(computedValue.value).toBe(1);
+    expect(getter).toHaveBeenCalledTimes(1);
+
+    // should not computed again
+    computedValue.value
+    expect(getter).toHaveBeenCalledTimes(1);
+
+    // should not computed until need
+    value.foo = 2;
+    expect(getter).toHaveBeenCalledTimes(1);
+
+    // now it should be computed
+    expect(computedValue.value).toBe(2);
+    expect(getter).toHaveBeenCalledTimes(2);
+
+    computedValue.value;
+    expect(getter).toHaveBeenCalledTimes(2);
+  })
+})
