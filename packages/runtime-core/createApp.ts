2cb289b0aa5da7763b8ba26564407c13639cabae
unbrain <marsorsun@gmail.com>
2024-04-02 23:59:09 +0800

    feat(runtime-dom): init runtime-dom

--- packages/runtime-core/createApp.ts
+++ packages/runtime-core/createApp.ts
@@ -1,16 +1,14 @@
-import { render } from "./render";
 import { createVNode } from "./vnode"
 
-
-
-export function createApp (rootComponent) {
-  return {
-    mount(rootContainer) {
-      // 先转换为 vnode 
-      // 
-      const vnode = createVNode(rootComponent)
-
-      render(vnode, rootContainer);
+export function createAppAPI(render) {
+  return function createApp(rootComponent) {
+    return {
+      mount(rootContainer) {
+        // 先转换为 vnode 
+        // 
+        const vnode = createVNode(rootComponent)
+        render(vnode, rootContainer);
+      }
     }
   }
 }
